2cded8aac1c3ae1d4084606ef62e53c97bf80b91
arjunsuwal <arjunsuwal900@gmail.com>
2025-08-16 23:09:27 -0400

    Added wsgi.py and modified

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
+# Create Flask app
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# This is what Elastic Beanstalk expects
+application = app
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
